fac73e49380a00396191185f1ba1d23b1efe86d0
nazaporta <nazarena@porta.com.py>
2025-05-11 13:21:57 -0300

    add api key

--- src/MovieApp.jsx
+++ src/MovieApp.jsx
@@ -5,8 +5,8 @@ export const MovieApp = () => {
   const [search, setSearch] = useState("");
   const [movieList, setMovieList] = useState(null);
 
-  const urlBase = "";
-  const API_KEY = "";
+  const urlBase = "https://api.themoviedb.org/3/search/movie";
+  const API_KEY = "f957da2832b7b1a9ea846529ca376fbc";
 
   const handleInputChange = (event) => {
     setSearch(event.target.value);
@@ -14,19 +14,22 @@ export const MovieApp = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    fetchMovies();
   };
 
   const fetchMovies = async () => {
     try {
-        const response = await fetch(`${urlBase}?query=${search}&api_key=${API_KEY}`)
-        const data = await response.json()
-        setMovieList(data.results)
-        console.log(data.results);
+      const response = await fetch(
+        `${urlBase}?query=${search}&api_key=${API_KEY}`
+      );
+      const data = await response.json();
+      setMovieList(data.results);
+      console.log(data.results);
     } catch (error) {
-        console.error('Message error: ', error)
+      console.error("Message error: ", error);
     }
-  }
-  
+  };
+
   return (
     <>
       <div className="container">
@@ -36,26 +39,28 @@ export const MovieApp = () => {
           <input
             type="text"
             placeholder="Search a movie"
-            name=""
-            id=""
             value={search}
             onChange={handleInputChange}
           />
 
           <button>Search</button>
 
-          {movieList && 
+          {movieList && (
             <div className="movie-list">
-              {movieList.map(movie =>{
+              {movieList.map((movie) => (
                 <div key={movie.id} className="movie-card">
-                  <img src={`${movie.poster_path}`} alt={movie.title} />
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                  />
                   <h2>{movie.title}</h2>
                   <p>{movie.overview}</p>
                 </div>
-              })}
+              ))}
             </div>
-          }
+          )}
         </form>
+
       </div>
     </>
   );
